3650d9ee968bc9ed1ab7b716f5968381ddb9eecf
bmatis <matis.brun@etu.univ-lyon1.fr>
2022-03-24 15:33:52 +0100

    .cpp de joueur et magasin

--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -1,1 +1,13 @@
 #include "Joueur.h"
+
+unsigned int Joueur::getLifes() {
+    return nb_vies;
+}
+
+unsigned int Joueur::setLifes(int life) {
+    return nb_vies = life;
+}
+
+void Joueur::removesLifes() {
+    nb_vies = nb_vies - 1;
+}
--- src/magasin.h
+++ src/magasin.h
@@ -4,12 +4,11 @@ class Magasin {
 
     private:
         unsigned int money; 
-        Game game;
 
     public:
-        void acheterDef(); /** \brief permet au joueur d'acheter des défenses **/
-        void vendreDef(); /** \brief permet au joueur de vendre des défenses **/
+        void acheterDef(typeDef type); /** \brief permet au joueur d'acheter des défenses **/
+        void vendreDef(typeDef type); /** \brief permet au joueur de vendre des défenses **/
         unsigned int getMoney(); /** \brief lis le nombre d'argent du joueur **/
-        unsigned int addMoney(int moula) /** \brief ajoute de l'argent au compteur du joueur **/
+        unsigned int addMoney(Monstre type); /** \brief ajoute de l'argent au compteur du joueur **/
 
 };
