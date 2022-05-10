8f706953009520a2031a775e8dbf11e3609cf409
bmatis <matis.brun@etu.univ-lyon.fr>
2022-05-10 09:04:35 +0200

    menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -53,18 +53,39 @@ void Menu::MenuInit(){
 
 }
 
+//Affiche du texte selon l'entrée 
+void Menu::AfficherTexteMenu(string Msg, string MsgWithValeur, float Valeur, int x, int y, int w, int h, int r, int g, int b){
+
+    SDL_Color color = { r, g, b };
+
+    const char* text = Msg.c_str();
+
+    if (Msg == ""){
+        ostringstream Val;
+        Val << Valeur;
+        string val = MsgWithValeur + Val.str();
+        text = val.c_str();
+    }
+
+    SDL_Surface * surface = TTF_RenderText_Solid(font, text, color);
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+    SDL_Rect dstrect = { x, y, w, h };
+    SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+    
+}
+
 void Menu::MenuAfficher(){
 
-    font_color.r = 80;font_color.g = 50;font_color.b = 255;
-	font_im.setSurface(TTF_RenderText_Solid(font,"Jouer",font_color));
-	font_im.loadFromCurrentSurface(renderer);
-    // Ecrire un titre par dessus
-    SDL_Rect position;
-    position.x = 270; position.y = 49; position.w = 100; position.h = 30;
-    SDL_RenderCopy(renderer,font_im.getTexture(),nullptr,&position);
+	AfficherTexteMenu("Jouer","",0,270,49,100,30,50,50,250);
+    AfficherTexteMenu("Options","",0,270,249,100,30,250,50,250);
+    AfficherTexteMenu("Scores","",0,270,49,100,30,50,250,250);
 
-
-    font_color2.r = 50;font_color2.g = 250;font_color2.b = 255;
+    /* font_color2.r = 50;font_color2.g = 250;font_color2.b = 255;
     font_im2.setSurface(TTF_RenderText_Solid(font,"Options",font_color2));
 	font_im2.loadFromCurrentSurface(renderer);
     // options
@@ -72,9 +93,17 @@ void Menu::MenuAfficher(){
     position2.x = 270; position2.y = 249; position2.w = 100; position2.h = 30;
     SDL_RenderCopy(renderer,font_im2.getTexture(),nullptr,&position2); 
 
+    font_color3.r = 50;font_color3.g = 250;font_color3.b = 255;
+    font_im3.setSurface(TTF_RenderText_Solid(font,"Options",font_color3));
+	font_im3.loadFromCurrentSurface(renderer);
+    // options
+    SDL_Rect position3;
+    position3.x = 270; position3.y = 249; position3.w = 100; position3.h = 30;
+    SDL_RenderCopy(renderer,font_im2.getTexture(),nullptr,&position3);  */
+
 }
 
-/* void Menu::MenuBoucle(){
+void Menu::MenuBoucle(){
     bool display=true;
     int xMouse, yMouse;
 
@@ -88,7 +117,18 @@ void Menu::MenuAfficher(){
             
             if (events.type == SDL_QUIT) display = false;
 
-                    
+            if(events.type == SDL_MOUSEBUTTONDOWN) {
+                SDL_GetMouseState(&xMouse,&yMouse);
+                if(xMouse > 270 && yMouse > 49 && xMouse < 370 && yMouse < 79) {
+                    //lancer le jeu quand on appuie sur jouer
+                }
+            }
+            if(events.type == SDL_MOUSEBUTTONDOWN) {
+                SDL_GetMouseState(&xMouse,&yMouse);
+                if(xMouse > 270 && yMouse > 249 && xMouse < 370 && yMouse < 279) {
+                    //ouvre le menu d'options 
+                }
+            }
         MenuAfficher();
         SDL_RenderPresent(renderer);
 
@@ -97,7 +137,7 @@ void Menu::MenuAfficher(){
         if(clear != true ) SDL_RenderClear(renderer);
     }
      
-  } */
+} 
 
 
 
